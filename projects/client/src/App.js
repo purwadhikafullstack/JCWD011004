a59819e66ea486a0a5414e70d3a04aa0c5a381a5
Gian Septian Lattang <gianseptianlattang@gmail.com>
2023-09-06 20:26:32 +0700

    add route setting

--- projects/client/src/App.js
+++ projects/client/src/App.js
@@ -1,31 +1,15 @@
-import React, { useState } from 'react';
-import LoginModal from '../src/components/loginModal/loginModal'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
 
-
-const App = () => {
-  const [isLoginModalOpen, setLoginModalOpen] = useState(false);
-
-  const handleOpenLoginModal = () => {
-    setLoginModalOpen(true);
-  };
-
-  const handleCloseLoginModal = () => {
-    setLoginModalOpen(false);
-  };
-
+function App() {
   return (
-    <div>
-      <h1>Contoh Aplikasi React</h1>
-      <button
-        className="bg-blue-500 text-white rounded-full py-2 px-4 hover:bg-blue-600 focus:outline-none"
-        onClick={handleOpenLoginModal}
-      >
-        Buka Modal Login
-      </button>
-
-      <LoginModal isOpen={isLoginModalOpen} onClose={handleCloseLoginModal} />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
