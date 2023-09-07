708ee0058daaeee816e545b1cebe4263163690e0
Gian Septian Lattang <gianseptianlattang@gmail.com>
2023-09-07 10:56:32 +0700

    Update banner.jsx

--- projects/client/src/components/homePage/banner.jsx
+++ projects/client/src/components/homePage/banner.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div className="mt-16 bg-orange-100 p-6 text-black">
+    <div className="mt-16 bg-orange-100 px-4 py-6 text-black">
       <div className="container mx-auto">
-        <p className="text-center pb-4">
+        <p className="text-center pb-3">
           Welcome to AKUI website! Special offer: 10% off everything - Use code
           AKUIAKU10%
         </p>
