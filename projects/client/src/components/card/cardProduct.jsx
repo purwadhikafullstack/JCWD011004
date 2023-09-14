cd4047ad37fef0f0c0c22b4b9a416e4830030a92
AndreAdyatmoko <AndreAdyatmoko>
2023-09-14 19:26:02 +0700

    y

--- projects/client/src/components/card/cardProduct.jsx
+++ projects/client/src/components/card/cardProduct.jsx
@@ -1,42 +1,41 @@
-// AddToCartCard.js
-
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Card() {
+  const handleAddToCart = () => {
+    alert('Item added to cart!')
+  }
+
   return (
-    <>
-      <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center">
+      <Link to="/product/:id">
         <div className="p-3 m-3 bg-white rounded-lg shadow-lg w-40 lg:w-56 h-auto hover:border border-gray-300 transition duration-300 ease-in-out">
-          <a href="#">
-            <div className="p-3">
-              <img
-                src="/card/chair.png"
-                alt="Product Image"
-                className="m-auto w-36 h-36 object-cover rounded-t-lg"
-              />
-            </div>
-            <div className="text-center">
-              <h2 className="mb-4 text-xl font-semibold">Product Title</h2>
-              <p className="mb-4 text-gray-600 truncate">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-                reiciendis ullam.
-              </p>
-              <p className="mb-4 text-m font-bold">IDR 300.000</p>
-            </div>
-          </a>
+          <div className="p-3">
+            <img
+              src="/card/chair.png"
+              alt="Product Image"
+              className="m-auto w-36 h-36 object-cover rounded-t-lg"
+            />
+          </div>
+          <div className="text-center">
+            <h2 className="mb-4 text-xl font-semibold">Product Title</h2>
+            <p className="mb-4 text-gray-600 truncate">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+              reiciendis ullam.
+            </p>
+            <p className="mb-4 text-m font-bold">IDR 300.000</p>
+          </div>
           <div className="pb-3 text-center">
             <button
               className="px-4 py-2 bg-orange-300 text-black rounded-full hover:bg-orange-400 focus:outline-none"
-              onClick={() => {
-                alert('Item added to cart!')
-              }}
+              onClick={handleAddToCart}
             >
               Add to Cart
             </button>
           </div>
         </div>
-      </div>
-    </>
+      </Link>
+    </div>
   )
 }
 
--- projects/client/src/components/card/cardProductDetail.jsx
+++ projects/client/src/components/card/cardProductDetail.jsx
@@ -35,12 +35,12 @@ function ProductDetailPage({ id }) {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-full bg-white rounded-lg overflow-y-auto m-3 p-3">
+      <div className="w-full bg-white rounded-lg overflow-y-auto m-3 p-3 shadow-lg">
         <div className="p-3 justify-center items-center">
           <img
             src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/281/0728155_PE736115_S4.webp"
             alt="Product Image"
-            className="mx-auto block w-250 h-250 object-cover rounded-t-lg border-radius-lg"
+            className="mx-auto block w-250 h-250 object-cover rounded-t-lg"
           />
         </div>
         <div className="text-left p-3">
@@ -49,7 +49,7 @@ function ProductDetailPage({ id }) {
           <p className="mb-4 text-m font-bold">IDR {productData.price}</p>
           <div className="mt-4 flex justify-between items-center">
             <button
-              className="px-3 py-2 bg-orange-300 text-black rounded-full hover:bg-orange-400 focus:outline-none"
+              className="px-4 py-2 bg-orange-300 text-black rounded-full hover:bg-orange-400 focus:outline-none"
               onClick={handleAddToCart}
             >
               Add to Cart
