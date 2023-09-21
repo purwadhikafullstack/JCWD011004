0c261c65332d87341f15698998b8f49e3e4a437d
dikokartiko <124632027+dikokartiko@users.noreply.github.com>
2023-09-22 02:39:39 +0700

    As a non-registered or non-verified user, I cannot do any sort of transaction (#20)

--- projects/client/src/components/card/components/AddToCartButton.jsx
+++ projects/client/src/components/card/components/AddToCartButton.jsx
@@ -1,15 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import LoginModal from '../../loginModal/loginModal'
 
 function AddToCartButton() {
+  const [openLoginModal, setLoginModalOpen] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  const handleLoginModal = () => {
+    setLoginModalOpen(!openLoginModal)
+  }
+  const handleAddToCart = () => {
+    setIsDisabled(true)
+    const token = localStorage.getItem('token')
+    !token
+      ? (() => {
+          setLoginModalOpen(true)
+          toast.error('Silahkan login terlebih dahulu', {
+            position: toast.POSITION.TOP_CENTER
+          })
+          setIsDisabled(false)
+        })()
+      : toast.success('Product ditambahkan di keranjang', {
+          position: toast.POSITION.TOP_CENTER
+        })
+    setIsDisabled(false)
+  }
+
   return (
-    <button
-      className="px-4 py-2 bg-orange-300 text-black rounded-full hover:bg-orange-400 focus:outline-none"
-      onClick={() => {
-        alert('Item added to cart!')
-      }}
-    >
-      Add to Cart
-    </button>
+    <>
+      <button
+        disabled={isDisabled}
+        className="px-4 py-2 bg-orange-300 text-black rounded-full hover:bg-orange-400 focus:outline-none"
+        onClick={handleAddToCart}
+      >
+        Add to Cart
+      </button>
+      <LoginModal isOpen={openLoginModal} onClose={handleLoginModal} />
+      <ToastContainer />
+    </>
   )
 }
 
