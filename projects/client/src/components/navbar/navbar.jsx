4fa108d70bfd62c3987872e7e5d2a189c3cb8bd6
Gian Septian Lattang <81907224+gianseptianlattang@users.noreply.github.com>
2023-09-21 23:11:09 +0700

    Mwect4 fixing avatar and sorting (#18)
    * change sort
    
    * done fixing avatar and sort
    
    * add dependency and responsive banner
    
    * Update .gitignore

--- projects/client/src/components/navbar/navbar.jsx
+++ projects/client/src/components/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FiShoppingCart } from 'react-icons/fi'
-import { RxAvatar } from 'react-icons/rx'
 import { AiOutlineHeart } from 'react-icons/ai'
 import ButtonLoginRegister from '../homePage/buttonLoginRegister'
 import { useSelector } from 'react-redux'
+import { AvatarDropdown } from './profile'
 
 function Navbar() {
   const stateToken = useSelector((state) => state.dataProduct.isLogin)
@@ -12,27 +12,29 @@ function Navbar() {
     <div className="container mx-auto max-w-screen-lg fixed top-0 bg-gray-900 text-white px-7 py-5 z-50">
       <nav>
         <div className="container mx-auto">
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="text-3xl font-bold">
               <a href="/">AKUI</a>
             </div>
-            <div className="flex space-x-6 items-center">
+            <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-6 items-center">
               <input
                 type="text"
                 placeholder="Search Product..."
                 className="bg-gray-700 text-white px-3 py-1 rounded-lg focus:outline-none hover:border border-gray-500"
               />
               <ul className="flex space-x-6">
-                <li className="flex justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
-                  <AiOutlineHeart /> Wishlist
+                <li className="flex flex-col md:flex-row justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
+                  <AiOutlineHeart className="md:mr-2" />
+                  Wishlist
                 </li>
-                <li className="flex justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
-                  <FiShoppingCart /> Cart
+                <li className="flex flex-col md:flex-row justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
+                  <FiShoppingCart className="md:mr-2" />
+                  Cart
                 </li>
-                <li className="flex justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
+                <li className="flex flex-col md:flex-row justify-between items-center hover:text-gray-400 transition duration-300 ease-in-out">
                   {stateToken ? (
                     <>
-                      <RxAvatar /> Profile
+                      <AvatarDropdown />
                     </>
                   ) : (
                     <ButtonLoginRegister />
---
+++ projects/client/src/components/navbar/profile.jsx
@@ -0,0 +1,94 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export const AvatarDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [avatar, setAvatar] = useState(null)
+  const navigate = useNavigate()
+
+  const fetchImage = async () => {
+    try {
+      const token = localStorage.getItem('token')
+      const response = await axios.get(
+        'http://localhost:8000/api/update/avatars',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      )
+      setAvatar(response.data)
+    } catch (error) {
+      setAvatar(null)
+    }
+  }
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleProfile = () => {
+    navigate('/user-dashboard')
+  }
+
+  const handleSignOut = () => {
+    localStorage.clear()
+    navigate('/')
+    window.location.reload()
+  }
+
+  useEffect(() => {
+    fetchImage()
+  }, [])
+
+  return (
+    <div className="relative inline-block text-left">
+      <button
+        onClick={toggleDropdown}
+        className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-300 hover:bg-orange-300"
+      >
+        <img
+          src={
+            avatar
+              ? avatar?.avatar
+              : 'https://img.icons8.com/pastel-glyph/512/person-male--v3.png'
+          }
+          alt="Avatar"
+          className="w-9 h-9 rounded-full"
+        />
+      </button>
+      {isOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+          <div
+            className="py-1"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            <button
+              onClick={handleProfile}
+              className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+            >
+              Profile
+            </button>
+            <button
+              className="block w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+              role="menuitem"
+            >
+              Settings
+            </button>
+            <button
+              onClick={handleSignOut}
+              className="block w-full px-4 py-2 text-sm text-red-600 hover:bg-gray-100 hover:text-red-800"
+              role="menuitem"
+            >
+              Sign out
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
