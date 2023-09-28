b45ac397275854b51b90a71db68a0eca3978fc56
dikokartiko <dikowebdev@gmail.com>
2023-09-29 02:04:19 +0700

    fix database warehouse_product models

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -22,7 +22,7 @@ app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')))
 
 app.use(express.json())
 // const db = require('../models')
-// db.sequelize.sync({ alter: true })
+// db.sequelize.sync({ force: true })
 
 //#region API ROUTES
 
