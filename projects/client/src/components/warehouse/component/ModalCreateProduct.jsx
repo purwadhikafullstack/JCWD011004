85fe5ffdf08668f08ab05f19d8d70b6ff0fc5665
AndreAdyatmoko <AndreAdyatmoko>
2023-10-02 13:06:14 +0700

    Fix Modal

--- projects/client/src/components/warehouse/component/ModalCreateProduct.jsx
+++ projects/client/src/components/warehouse/component/ModalCreateProduct.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Modal from 'react-modal'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 // Komponen modal untuk membuat gudang
 function CreateWarehouseModal() {
@@ -8,17 +10,73 @@ function CreateWarehouseModal() {
   const [selectedProvince, setSelectedProvince] = useState('')
   const [cities, setCities] = useState([])
   const [selectedCity, setSelectedCity] = useState('')
+  const [subdistrict, setSubdistrict] = useState('') // State untuk kecamatan
+  const [warehouseName, setWarehouseName] = useState('') // State untuk nama Warehouse
+  const [streetAddress, setStreetAddress] = useState('') // State untuk jalan
+  const [postalCode, setPostalCode] = useState('') // State untuk kode pos
+  const [isLoading, setIsLoading] = useState(false) // State untuk status loading
+  const [isCreating, setIsCreating] = useState(false) // State untuk status "Buat"
 
   // Fungsi untuk menutup modal
   const closeModal = () => {
     setSelectedProvince('')
     setSelectedCity('')
+    setSubdistrict('')
+    setWarehouseName('')
+    setStreetAddress('')
+    setPostalCode('')
     setIsOpen(false) // Tutup modal dengan mengubah state isOpen menjadi false
   }
 
   // Fungsi untuk membuka modal
   const openModal = () => {
     setIsOpen(true) // Buka modal dengan mengubah state isOpen menjadi true
+  }
+
+  // Fungsi untuk mengirimkan data gudang (dummy data untuk simulasi)
+  const createWarehouse = () => {
+    // Validasi jika semua field terisi
+    if (
+      !warehouseName ||
+      !streetAddress ||
+      !selectedProvince ||
+      !selectedCity ||
+      !subdistrict ||
+      !postalCode
+    ) {
+      toast.error('Harap isi semua field.', {
+        position: toast.POSITION.TOP_CENTER
+      })
+      return
+    }
+
+    // Set status "Buat" menjadi true saat tombol ditekan
+    setIsCreating(true)
+
+    // Simulasi proses pengiriman data gudang
+    setIsLoading(true) // Set loading menjadi true saat proses berjalan
+
+    setTimeout(() => {
+      // Simulasi sukses membuat gudang
+      setIsLoading(false) // Set loading menjadi false setelah berhasil
+      setIsCreating(false) // Set status "Buat" menjadi false
+
+      // Reset form
+      setSelectedProvince('')
+      setSelectedCity('')
+      setSubdistrict('')
+      setWarehouseName('')
+      setStreetAddress('')
+      setPostalCode('')
+
+      // Tutup modal
+      closeModal()
+
+      // Tampilkan pesan toast sukses
+      toast.success('Gudang berhasil dibuat!', {
+        position: toast.POSITION.TOP_CENTER
+      })
+    }, 2000) // Simulasi waktu proses
   }
 
   // Fungsi untuk mengambil data provinsi (dummy data)
@@ -47,11 +105,41 @@ function CreateWarehouseModal() {
         isOpen={isOpen}
         onRequestClose={closeModal}
         contentLabel="Create Warehouse Modal"
-        className="modal"
+        className="modal w-[400px] h-[300px] bg-white p-4 rounded-lg shadow-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
         overlayClassName="overlay" // Gaya overlay dengan Tailwind CSS
       >
         <div className="bg-white p-4 rounded-lg shadow-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <h2 className="text-2xl font-semibold mb-4">Create Warehouse</h2>
+          <label className="block mb-2">
+            Nama Warehouse:
+            <input
+              type="text"
+              className="border rounded-md p-2 w-full"
+              value={warehouseName}
+              onChange={(e) => setWarehouseName(e.target.value)}
+            />
+          </label>
+          <br />
+          <label className="block mb-2">
+            Jalan:
+            <input
+              type="text"
+              className="border rounded-md p-2 w-full"
+              value={streetAddress}
+              onChange={(e) => setStreetAddress(e.target.value)}
+            />
+          </label>
+          <br />
+          <label className="block mb-2">
+            Kode Pos:
+            <input
+              type="text"
+              className="border rounded-md p-2 w-full"
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </label>
+          <br />
           <label className="block mb-2">
             Provinsi:
             <select
@@ -84,14 +172,38 @@ function CreateWarehouseModal() {
             </select>
           </label>
           <br />
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
-            onClick={closeModal}
-          >
-            Tutup
-          </button>
+          <label className="block mb-2">
+            Kecamatan:
+            <input
+              type="text"
+              className="border rounded-md p-2 w-full"
+              value={subdistrict}
+              onChange={(e) => setSubdistrict(e.target.value)}
+            />
+          </label>
+          <br />
+          <div className="flex justify-between">
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+              onClick={closeModal}
+              disabled={isLoading} // Tombol hanya aktif jika tidak dalam status loading
+            >
+              Tutup
+            </button>
+            <button
+              className={`bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ${
+                isLoading || isCreating ? 'opacity-50 cursor-not-allowed' : ''
+              }`}
+              onClick={isLoading || isCreating ? null : createWarehouse} // Tombol hanya aktif jika tidak dalam status loading atau status "Buat"
+            >
+              {isLoading ? 'Membuat...' : isCreating ? 'Membuat' : 'Buat'}
+            </button>
+          </div>
+          <br />
+          {isLoading && <p>Sedang memproses...</p>}
         </div>
       </Modal>
+      <ToastContainer />
     </div>
   )
 }
---
+++ projects/client/src/components/warehouse/component/ModalEditProduct.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react'
+import Modal from 'react-modal'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+// Komponen modal untuk mengedit gudang
+function EditWarehouseModal({ isOpen, onClose, warehouseData }) {
+  const [editedWarehouse, setEditedWarehouse] = useState(warehouseData)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setEditedWarehouse(warehouseData)
+  }, [warehouseData])
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setEditedWarehouse((prevData) => ({
+      ...prevData,
+      [name]: value
+    }))
+  }
+
+  const handleEditWarehouse = () => {
+    // Validasi jika semua field terisi
+    if (
+      !editedWarehouse.warehouseName ||
+      !editedWarehouse.streetAddress ||
+      !editedWarehouse.selectedProvince ||
+      !editedWarehouse.selectedCity
+    ) {
+      toast.error('Harap isi semua field.', {
+        position: toast.POSITION.TOP_CENTER
+      })
+      return
+    }
+
+    setIsLoading(true)
+
+    // Simulasi proses pengiriman data gudang
+    setTimeout(() => {
+      setIsLoading(false)
+
+      // Tutup modal
+      onClose()
+
+      // Tampilkan pesan toast sukses
+      toast.success('Gudang berhasil diubah!', {
+        position: toast.POSITION.TOP_CENTER
+      })
+    }, 2000) // Simulasi waktu proses
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Edit Warehouse Modal"
+      className="modal"
+      overlayClassName="overlay"
+    >
+      <div className="bg-white p-4 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-semibold mb-4">Edit Warehouse</h2>
+        <label className="block mb-2">
+          Nama Warehouse:
+          <input
+            type="text"
+            className="border rounded-md p-2 w-full"
+            name="warehouseName"
+            value={editedWarehouse.warehouseName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label className="block mb-2">
+          Jalan:
+          <input
+            type="text"
+            className="border rounded-md p-2 w-full"
+            name="streetAddress"
+            value={editedWarehouse.streetAddress}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label className="block mb-2">
+          Provinsi:
+          <select
+            className="border rounded-md p-2 w-full"
+            name="selectedProvince"
+            value={editedWarehouse.selectedProvince}
+            onChange={handleInputChange}
+          >
+            <option value="">Pilih Provinsi</option>
+            {/* Render the list of provinces */}
+          </select>
+        </label>
+        <br />
+        <label className="block mb-2">
+          Kabupaten:
+          <select
+            className="border rounded-md p-2 w-full"
+            name="selectedCity"
+            value={editedWarehouse.selectedCity}
+            onChange={handleInputChange}
+          >
+            <option value="">Pilih Kabupaten</option>
+            {/* Render the list of cities */}
+          </select>
+        </label>
+        <br />
+        <div className="flex justify-between">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+            onClick={onClose}
+            disabled={isLoading}
+          >
+            Tutup
+          </button>
+          <button
+            className={`bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 ${
+              isLoading ? 'opacity-50 cursor-not-allowed' : ''
+            }`}
+            onClick={isLoading ? null : handleEditWarehouse}
+          >
+            {isLoading ? 'Mengedit...' : 'Edit'}
+          </button>
+        </div>
+        <br />
+        {isLoading && <p>Sedang memproses...</p>}
+      </div>
+    </Modal>
+  )
+}
+
+export default EditWarehouseModal
