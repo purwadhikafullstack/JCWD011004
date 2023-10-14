aa8a7379990c99686eccd9b238e14af2399d6c30
AndreAdyatmoko <AndreAdyatmoko>
2023-10-14 20:25:09 +0700

    tidak jadi

--- projects/server/src/controller/admin/getTransactionAdminWarehouse.js
+++ projects/server/src/controller/admin/getTransactionAdminWarehouse.js
@@ -4,10 +4,13 @@ const Product = db.Product
 const Warehouse_Admin = db.Warehouse_Admin
 const Warehouse = db.Warehouse
 const Transaction_Item = db.Transaction_Item
+const User = db.User
 
 const getAllTransactionAdmin = async (req, res) => {
   try {
     const { id } = req.params
+    const { transactionStatusId } = req.query // Ambil parameter transactionStatusId dari query string
+
     const warehouseAdmin = await Warehouse_Admin.findOne({
       where: { adminId: id }
     })
@@ -18,11 +21,22 @@ const getAllTransactionAdmin = async (req, res) => {
         .json({ message: 'Anda tidak memiliki izin akses ke gudang.' })
     }
 
+    const whereCondition = {
+      warehouseId: warehouseAdmin.warehouseId
+    }
+
+    if (transactionStatusId !== undefined) {
+      // Jika transactionStatusId disertakan dalam query string, tambahkan filter berdasarkan transactionStatusId
+      whereCondition.transactionStatusId = transactionStatusId
+    }
+
     const allTransaction = await Transaction.findAll({
-      where: { warehouseId: warehouseAdmin.warehouseId },
+      where: whereCondition,
       include: [
         { model: Warehouse },
-        {model: Transaction_Item, include: Product}]
+        { model: Transaction_Item, include: Product },
+        { model: User }
+      ]
     })
 
     return res.status(200).json({ allTransaction })
