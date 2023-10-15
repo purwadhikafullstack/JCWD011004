2b23940ea7879cfb53392feb2885eb312a1a1223
dikokartiko <dikowebdev@gmail.com>
2023-10-16 00:54:15 +0700

    add refactor

--- projects/server/src/controller/transaction/confirmationPayment.js
+++ projects/server/src/controller/transaction/confirmationPayment.js
@@ -10,41 +10,46 @@ const rejectPayment = async (req, res) => {
     if (!transaction) {
       return res.status(404).json({ message: 'Transaction not found' })
     }
-
-    transaction.transactionStatusId = 0
-    await transaction.save()
-
-    // Nodemailer configuration
-    let transporter = nodemailer.createTransport({
-      service: process.env.NODEMAILER_SERVICE,
-      auth: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASS
-      }
-    })
-
-    // Email options
-    let mailOptions = {
-      from: process.env.NODEMAILER_USER,
-      to: 'recipient@example.com', // replace with recipient email
-      subject: 'Payment Rejection',
-      text: 'Your payment has been rejected.'
-    }
-
-    // Send email
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log('Email sent: ' + info.response)
-      }
-    })
-
+    await updateTransactionStatus(transaction, 0)
+    await sendRejectionEmail()
     res.json({ message: 'Payment rejected successfully' })
   } catch (err) {
     console.error(err)
     res.status(500).json({ message: 'Server error' })
   }
+}
+
+const updateTransactionStatus = async (transaction, statusId) => {
+  transaction.transactionStatusId = statusId
+  await transaction.save()
+}
+
+const sendRejectionEmail = async () => {
+  // Nodemailer configuration
+  let transporter = nodemailer.createTransport({
+    service: process.env.NODEMAILER_SERVICE,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASS
+    }
+  })
+
+  // Email options
+  let mailOptions = {
+    from: process.env.NODEMAILER_USER,
+    to: 'recipient@example.com', // replace with recipient email
+    subject: 'Payment Rejection',
+    text: 'Your payment has been rejected, send the payment proof with good quality and clear image.'
+  }
+
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
 }
 
 module.exports = {
