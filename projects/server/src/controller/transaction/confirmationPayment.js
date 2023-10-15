a20096a75e12b8d7b7606ff0f1abb6ef5cdadcc5
dikokartiko <dikowebdev@gmail.com>
2023-10-16 00:50:15 +0700

    add email

--- projects/server/src/controller/transaction/confirmationPayment.js
+++ projects/server/src/controller/transaction/confirmationPayment.js
@@ -1,3 +1,4 @@
+const nodemailer = require('nodemailer')
 const db = require('../../../models')
 const Transaction = db.Transaction
 
@@ -12,6 +13,32 @@ const rejectPayment = async (req, res) => {
 
     transaction.transactionStatusId = 0
     await transaction.save()
+
+    // Nodemailer configuration
+    let transporter = nodemailer.createTransport({
+      service: process.env.NODEMAILER_SERVICE,
+      auth: {
+        user: process.env.NODEMAILER_USER,
+        pass: process.env.NODEMAILER_PASS
+      }
+    })
+
+    // Email options
+    let mailOptions = {
+      from: process.env.NODEMAILER_USER,
+      to: 'recipient@example.com', // replace with recipient email
+      subject: 'Payment Rejection',
+      text: 'Your payment has been rejected.'
+    }
+
+    // Send email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
 
     res.json({ message: 'Payment rejected successfully' })
   } catch (err) {
