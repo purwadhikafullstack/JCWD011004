a48c7c3aa73a681cec9b3986b61eb27bd96355d3
High Call <49810289+haikal1223@users.noreply.github.com>
2023-10-29 14:22:37 +0700

    Update index.js

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -31,7 +31,7 @@ app.use(
     ]
   })
 )
-app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')))
+app.use('/api/uploads', express.static(path.join(__dirname, '../public/uploads')))
 
 app.use(express.json())
 
